608fc1036ae0f76c8a7e44340b70e8ed6353e577
Прохватаева Анна <Anna89851004453@yandex.ru>
2022-05-22 17:03:33 +0300

    Добавлены файлы

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,23 @@
+public class BinOps {
+    public String sum(String a, String b) {
+        int aInt = Integer.parseInt(a, 2);
+        int bInt = Integer.parseInt(b, 2);
+        int sum = aInt + bInt;
+        String result = Integer.toBinaryString(sum);
+        return result;
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+    }
+
+    public String mult(String a, String b) {
+        int aInt = Integer.parseInt(a, 2);
+        int bInt = Integer.parseInt(b, 2);
+        int mult = aInt * bInt;
+        String result = Integer.toBinaryString(mult);
+        return result;
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("0111", "1110"));
+        System.out.println(bins.mult("1111", "1010"));
+
+
+    }
+}
